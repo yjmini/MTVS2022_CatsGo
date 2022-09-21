d0c4972a91d0a5e8b49381721d1f0d2de47d8e56
yjmini <u79jm@koreatech.ac.kr>
2022-09-21 14:37:02 +0900

    리듬게임 바 구현

---
+++ Assets/material/HealthGauge.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthGauge: MonoBehaviour
+{
+    Image healthBar;
+    float maxHealth = 100f;
+    public static float health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar = GetComponent<Image>();
+        health = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.fillAmount = health / maxHealth;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            HealthGauge.health += 10f;
+        }
+    }
+}
